535fff585ee3f66425c4a8a18cd1210b49372b50
Marco Leogrande <dark.knight.ita@gmail.com>
2016-03-29 09:17:14 -0700

    Project skeleton and initial protobuf file
    Signed-off-by: Marco Leogrande <dark.knight.ita@gmail.com>

---
+++ cmake/common.cmake
@@ -0,0 +1,34 @@
+#### protobuf section ####
+find_package(Protobuf REQUIRED)
+include_directories(${PROTOBUF_INCLUDE_DIRS})
+
+# Run c++ protocol buffer compiler on a proto file, generating source
+# and header files, plus a target name you can depend on.
+function(PROTOBUF_GENERATE_CPP SRC HDR PROTO_FILE TARGET_NAME)
+  set(${SRC})
+  set(${HDR})
+
+  get_filename_component(FILE_ABSOLUTE ${PROTO_FILE} ABSOLUTE)
+  get_filename_component(FILE_PATH ${FILE_ABSOLUTE} PATH)
+  get_filename_component(FILE_NOEXTENSION ${PROTO_FILE} NAME_WE)
+
+  list(APPEND ${SRC} "${CMAKE_CURRENT_BINARY_DIR}/${FILE_NOEXTENSION}.pb.cc")
+  list(APPEND ${HDR} "${CMAKE_CURRENT_BINARY_DIR}/${FILE_NOEXTENSION}.pb.h")
+  add_custom_command(
+    OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${FILE_NOEXTENSION}.pb.cc"
+    "${CMAKE_CURRENT_BINARY_DIR}/${FILE_NOEXTENSION}.pb.h"
+    COMMAND ${PROTOBUF_PROTOC_EXECUTABLE}
+    ARGS --cpp_out  ${CMAKE_CURRENT_BINARY_DIR} "${FILE_NOEXTENSION}.proto"
+    DEPENDS ${FILE_ABSOLUTE} ${FILE_NOEXTENSION}.proto
+    WORKING_DIRECTORY ${FILE_PATH}
+    COMMENT "Running C++ protoc on ${FILE_NOEXTENSION}.proto"
+    VERBATIM
+    )
+
+  # create a dependable target
+  add_custom_target(${TARGET_NAME} DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/${FILE_NOEXTENSION}.pb.cc")
+
+  set_source_files_properties(${${SRC}} ${${HDR}} PROPERTIES GENERATED TRUE)
+  set(${SRC} ${${SRC}} PARENT_SCOPE)
+  set(${HDR} ${${HDR}} PARENT_SCOPE)
+endfunction()
